b2030bd8b1a1e7a90235a14e62703f983346fd71
Helen Yanushevskaya <qahelping@gmail.com>
2024-07-18 19:21:06 +0300

    - add selenium example

--- diplom_wildberries/locators/favorites_locators.py
+++ diplom_wildberries/locators/favorites_locators.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+
+
 class FavouritesLocators:
     FAVORITES = (By.CSS_SELECTOR, 'class="favourites"')
     GO_SHOPPING_BUTTON = (By.XPATH, '//*[@data-text="strGoShopping"]')
